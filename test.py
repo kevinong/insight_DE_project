a60c0570f15f9ddbc133b18c8d2d5ffbeababab7
kevinong <kevinongs@gmail.com>
2018-04-26 13:39:18 -0700

    update test

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
+import findspark
+findspark.init()
+
 import os
-from pyspark import SparkConf, SparkContext
+# from pyspark import SparkConf, SparkContext
+import pyspark
 
 aws_access_key = os.getenv('AWS_ACCESS_KEY_ID', 'default')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY', 'default')
 
-conf = SparkConf().setAppName("test")
-sc = SparkContext(conf = conf)
+# conf = SparkConf().setAppName("test")
+# sc = SparkContext(conf = conf)
 
 
-path = "s3n://{}:{}@/product/metadata.json"
-data = sc.textFile(path)
+# path = "s3n://{}:{}@/product/metadata.json"
+# data = sc.textFile(path)
+bucket_name = "amazon-data-insight"
+folder_name = "product"
+file_name = "metadata.json"
 
-print(data)
+EC=sqlContext.read.format('json').options(header='true', inferSchema='true').load('s3a://{}/{}/{}/'.format(bucket_name, folder_name, file_name))
+# print(data)
+print(EC)
