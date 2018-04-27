27b93a63306ddd7fb502e95b31ebe087576c7155
Ubuntu <ubuntu@ip-10-0-0-7.us-west-2.compute.internal>
2018-04-27 01:18:44 +0000

    more test

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ findspark.init()
 
 import os
 from pyspark import SparkConf, SparkContext
-# import pyspark
 from pyspark.sql import SQLContext
 
 aws_access_key = os.getenv('AWS_ACCESS_KEY_ID', 'default')
@@ -12,18 +11,15 @@ aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY', 'default')
 conf = SparkConf().setAppName("test")
 sc = SparkContext(conf = conf)
 
-
-# path = "s3n://{}:{}@/product/metadata.json"
-# data = sc.textFile(path)
 bucket_name = "amazon-data-insight"
 folder_name = "product"
 file_name = "metadata.json"
 path = 's3a://{}/{}/{}/'.format(bucket_name, folder_name, file_name)
 
 sqlContext = SQLContext(sc)
-data = sqlContext.jsonFile(path)
 
-data.printSchema()
-# EC=sqlContext.read.format('json').options(header='true', inferSchema='true').load('s3a://{}/{}/{}/'.format(bucket_name, folder_name, file_name))
-# print(data)
-# print(EC)
+df = sqlContext.read.format('json').options(header='true', inferSchema='true').load('s3a://{}/{}/{}/'.format(bucket_name, folder_name, file_name))
+
+print("count: ", df.count())
+print("dtypes: ", df.dtypes)
+print("first row: ", df.first())
