ec7895cead93808a2288cfa4a2308749ec0cfcf6
kevinong <kevinongs@gmail.com>
2018-05-01 16:22:39 -0700

    separate polarity to pos and neg part 3

--- reviews_data.py
+++ reviews_data.py
@@ -46,8 +46,8 @@ class ReviewsData:
         # Transforming review data
         self.reviews_df = self.reviews_df\
                             .withColumn("polarity", polarity_udf(self.reviews_df.reviewText))\
-                            .withColumn("pos_polarity", pos_polarity_udf(col("polarity")))\
-                            .withColumn("neg_polarity", neg_polarity_udf(col("polarity")))\
+                            .withColumn("pos_polarity", pos_polarity_udf(functions.col("polarity")))\
+                            .withColumn("neg_polarity", neg_polarity_udf(functions.col("polarity")))\
                             .withColumn("subjectivity", subjectivity_udf(self.reviews_df.reviewText))\
                             .withColumn("helpful_vote", self.reviews_df.helpful[0])\
                             .withColumn("unhelpful_vote", self.reviews_df.helpful[1])
