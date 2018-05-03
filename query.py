107579d5173e2c90cc149c910828a3a38f84307f
kevinong <kevinongs@gmail.com>
2018-05-03 16:17:21 -0700

    try to query

---
+++ query.py
@@ -0,0 +1,12 @@
+from cassandra.cluster import Cluster
+
+CASSANDRA_SERVER    = ['54.245.66.232', '54.218.181.48', '54.71.237.54', '52.13.222.70']
+CASSANDRA_NAMESPACE = "AmazonReviews"
+
+cluster = Cluster(CASSANDRA_SERVER)
+session = cluster.connect(CASSANDRA_NAMESPACE)
+
+rows = session.execute('SELECT reviewerID, avg_star FROM data')
+
+for user_row in rows:
+    print user_row.reviewerID, user_row.avg_star
