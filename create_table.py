77b0de1e452a969192cf2c5ad71d6de735b4f5af
kevinong <kevinongs@gmail.com>
2018-05-03 13:58:23 -0700

    update cassandra code

--- create_table.py
+++ create_table.py
@@ -4,7 +4,12 @@ CASSANDRA_SERVER    = ['54.245.66.232', '54.218.181.48', '54.71.237.54', '52.13.
 CASSANDRA_NAMESPACE = "AmazonReviews"
 
 cluster = Cluster(CASSANDRA_SERVER)
-session = cluster.connect(CASSANDRA_NAMESPACE)
+session = cluster.connect()
+
+session.execute('DROP KEYSPACE IF EXISTS '+CASSANDRA_NAMESPACE + ';')
+
+session.execute('CREATE KEYSPACE ' + CASSANDRA_NAMESPACE  + ';')
+session.execute('USE ' + CASSANDRA_NAMESPACE)
 
 session.execute('DROP TABLE IF EXISTS data;')
 session.execute('CREATE TABLE data (reviewerID text, avg_star float, helpful float, pos float, neg float, PRIMARY KEY (reviewerID);')
