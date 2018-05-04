e73691c7fca59d487c89c4f2c32ae6f0dd082ff7
kevinong <kevinongs@gmail.com>
2018-05-03 19:15:15 -0700

    test ui

--- app.py
+++ app.py
@@ -6,19 +6,45 @@ import dash_html_components as html
 server = Flask(__name__)
 app = dash.Dash(__name__, server = server)
 
+from cassandra.cluster import Cluster
+
+CASSANDRA_SERVER    = ['54.245.66.232', '54.218.181.48', '54.71.237.54', '52.13.222.70']
+CASSANDRA_NAMESPACE = "AmazonReviews"
+
+cluster = Cluster(CASSANDRA_SERVER)
+session = cluster.connect()
+
+session.execute("USE " + CASSANDRA_NAMESPACE)
+
+rows = session.execute('SELECT reviewerid, avg_star FROM data')
+
+
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Find the right users for a product'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Amazon User Review Data
     '''),
+
+    # dcc.Graph(
+    #     id='example-graph',
+    #     figure={
+    #         'data': [
+    #             {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+    #             {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+    #         ],
+    #         'layout': {
+    #             'title': 'Dash Data Visualization'
+    #         }
+    #     }
+    # )
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2, 3], 'y': [rows[0].avg_star, rows[1].avg_star, rows[2].avg_star], 'type': 'bar', 'name': 'Stars'},
+                # {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
