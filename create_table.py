b64a3e4f3328e0614395728153331ff4bf1723df
kevinong <kevinongs@gmail.com>
2018-05-08 23:16:14 -0700

    modify user table schema

--- create_table.py
+++ create_table.py
@@ -1,7 +1,7 @@
 from cassandra.cluster import Cluster
 
-CASSANDRA_SERVER    = ['54.245.66.232', '54.218.181.48', '54.71.237.54', '52.13.222.70']
-CASSANDRA_NAMESPACE = "AmazonReviews"
+CASSANDRA_SERVER    = ['54.245.66.232', '34.214.245.150', '54.218.181.48', '54.71.237.54', '54.190.226.253', '35.165.118.115', '52.11.177.167', '34.215.123.166']
+CASSANDRA_NAMESPACE = "amazonreviews"
 
 cluster = Cluster(CASSANDRA_SERVER)
 session = cluster.connect()
@@ -11,8 +11,8 @@ session.execute('DROP KEYSPACE IF EXISTS '+CASSANDRA_NAMESPACE + ';')
 session.execute('CREATE KEYSPACE ' + CASSANDRA_NAMESPACE  + ' WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\' : 3};')
 session.execute('USE ' + CASSANDRA_NAMESPACE)
 
-session.execute('DROP TABLE IF EXISTS data;')
-session.execute('CREATE TABLE data (reviewerID text, avg_star float, helpful int, unhelpful int, avg_pol float, pos float, pos_review_count int, neg float, neg_review_count int, subjectivity float, categories set<text>, PRIMARY KEY (reviewerID));')
+session.execute('DROP TABLE IF EXISTS userdata;')
+session.execute('CREATE TABLE data (reviewerid text, avg_star float, helpful int, unhelpful int, avg_pol float, pos float, pos_review_count int, neg float, neg_review_count int, subjectivity float, PRIMARY KEY (reviewerid));')
 
 # grouped_df = self.df.groupby("reviewerid").agg(functions.avg("overall").alias("avg_star"), \
 #                                                                functions.sum("helpful_vote").alias("helpful"), \
