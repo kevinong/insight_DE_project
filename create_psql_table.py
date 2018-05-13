7d04b791e01ad60bb2b782ca8f42063360bac949
kevinong <kevinongs@gmail.com>
2018-05-12 19:41:39 -0700

    create tables

--- create_psql_table.py
+++ create_psql_table.py
@@ -4,10 +4,11 @@ import sys
 import os
 
 def create_tables():
-    command1 = "CREATE TABLE IF NOT EXISTS products2 (productid text PRIMARY KEY, productname text, categories text[]);"
-    # session.execute('CREATE TABLE userdata (reviewerid text, avg_star float, count int, helpful int, unhelpful int, avg_pol float, pos float, pos_review_count int, neg float, neg_review_count int, subjectivity float, PRIMARY KEY (reviewerid));')
-    # command = "CREATE TABLE IF NOT EXISTS users (reviewerid text PRIMARY KEY, avg_star float, count int, helpful int, unhelpful int, avg_pol float, pos float, pos_review_count int, neg float, neg_review_count int, subjectivity float);"
-    command2 = "CREATE TABLE IF NOT EXISTS users2 (reviewerid text PRIMARY KEY, avg_star float, count int, helpful int, unhelpful int);"
+
+    cats = ['books', 'electronics', 'moviestv', 'cdsvinyl', 'clothingshoesjewelry', 'homekitchen', 'kindlestore', 'sportsoutdoors', 'cellphonesaccessories', 'healthpersonalcare', 'toysgames', 'videogames', 'toolshomeimprovement', 'beauty', 'appsforandroid', 'officeproducts', 'petsupplies', 'automotive', 'grocerygourmetfood', 'patiolawngarden', 'baby', 'digitalmusic', 'musicalinstruments', 'amazoninstantvideo']
+
+    
+    # command2 = "CREATE TABLE IF NOT EXISTS users2 (reviewerid text PRIMARY KEY, avg_star float, count int, helpful int, unhelpful int);"
 
     postgres_url = 'postgresql://kevin:pw@ec2-54-245-66-232.us-west-2.compute.amazonaws.com:5432/insight'
 
@@ -21,9 +22,12 @@ def create_tables():
         print("got connection")
        # for command in commands:
        # print command
-        cur.execute(command1)
-        cur.execute(command2)
-        print("executed command")
+        for c in cats:
+            command1 = "CREATE TABLE IF NOT EXISTS " + c + "products (productid text PRIMARY KEY, productname text, categories text[]);"
+            command2 = "CREATE TABLE IF NOT EXISTS " + c + "users (reviewerid text PRIMARY KEY, avg_star float, count int, helpful int, unhelpful int, avg_pol float, pos float, pos_review_count int, neg float, neg_review_count int, subjectivity float);"
+            cur.execute(command1)
+            cur.execute(command2)
+            print("executed command")
         # close communication with the PostgreSQL database server
         cur.close()
         print("closed the cursor")
