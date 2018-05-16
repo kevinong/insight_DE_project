d93bda9262be20e90109a0de8e2d19f5abd33db5
kevinong <kevinongs@gmail.com>
2018-05-16 13:13:01 -0700

    refactor code

--- src/create_psql_table.py
+++ src/create_psql_table.py
@@ -5,42 +5,36 @@ import os
 
 def create_tables():
 
-    cats = ['books', 'electronics', 'moviestv', 'cdsvinyl', 'clothingshoesjewelry', 'homekitchen', 'kindlestore', 'sportsoutdoors', 'cellphonesaccessories', 'healthpersonalcare', 'toysgames', 'videogames', 'toolshomeimprovement', 'beauty', 'appsforandroid', 'officeproducts', 'petsupplies', 'automotive', 'grocerygourmetfood', 'patiolawngarden', 'baby', 'digitalmusic', 'musicalinstruments', 'amazoninstantvideo']
+    cats = ['books', 'electronics', 'moviestv', 'cdsvinyl', 
+            'clothingshoesjewelry', 'homekitchen', 'kindlestore', 
+            'sportsoutdoors', 'cellphonesaccessories', 'healthpersonalcare', 
+            'toysgames', 'videogames', 'toolshomeimprovement', 
+            'beauty', 'appsforandroid', 'officeproducts', 
+            'petsupplies', 'automotive', 'grocerygourmetfood', 
+            'patiolawngarden', 'baby', 'digitalmusic', 
+            'musicalinstruments', 'amazoninstantvideo']
 
     
-    # command2 = "CREATE TABLE IF NOT EXISTS users2 (reviewerid text PRIMARY KEY, avg_star float, count int, helpful int, unhelpful int);"
-
     postgres_url = 'postgresql://kevin:pw@ec2-54-245-66-232.us-west-2.compute.amazonaws.com:5432/insight'
 
     conn = None
 
     try:
-        # connect to the PostgreSQL server
         conn = psycopg2.connect(postgres_url)
         cur = conn.cursor()
-        # create table one by one
-        print("got connection")
-       # for command in commands:
-       # print command
         for c in cats:
             command1 = "CREATE TABLE IF NOT EXISTS " + c + "products (productid text PRIMARY KEY, productname text, categories text[]);"
             command2 = "CREATE TABLE IF NOT EXISTS " + c + "users (reviewerid text PRIMARY KEY, avg_star float, count int, helpful int, unhelpful int, avg_pol float, pos float, pos_review_count int, neg float, neg_review_count int, subjectivity float);"
             cur.execute(command1)
             cur.execute(command2)
             print("executed command")
-        # close communication with the PostgreSQL database server
         cur.close()
-        print("closed the cursor")
-        # commit the changes
         conn.commit()
-        print("committed the connection")
     except (Exception) as error:
-        print(error)
         raise error
     finally:
         if conn is not None:
             conn.close()
-            print("closed the connection")
             
 if __name__ == '__main__':
     create_tables()
---
+++ src/products_data.py
@@ -0,0 +1,55 @@
+import findspark
+findspark.init()
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext, functions
+from pyspark.sql.types import FloatType, ArrayType, StringType, IntegerType
+from cassandra.cluster import Cluster
+
+import configparser
+import psycopg2
+import sys
+
+import os
+from textblob import TextBlob
+import datetime
+
+from util import get_s3_path
+
+BUCKET = "amazon-data-insight"
+postgres_url = 'jdbc:postgresql://ec2-54-245-66-232.us-west-2.compute.amazonaws.com:5432/insight'
+postgres_properties = {'user': 'kevin', 'password':'pw'}
+
+
+def flat(input_list):
+    result = []
+    if input_list is None:
+        return result
+    for sublist in input_list:
+        try:
+            if sublist is not None:
+                result.append(sublist[0].strip().replace(',','').replace('&', '').replace(' ', '_').lower().encode('ascii'))
+        except:
+            pass
+
+    return result
+
+class ProductData:
+    def __init__(self, category, conf, sc):
+        path = get_s3_path(BUCKET, "product", "meta_{}.json".format(CATEGORIES[category]))
+        self.category = category
+        sqlContext = SQLContext(sc)
+        self.df = sqlContext.read.format('json').\
+            options(header='true', inferSchema='true').\
+            load(path)
+
+    def main(self):
+        self.df = self.df.select("asin", "title", "categories").na.drop(subset=["asin"])
+
+        flat_udf = functions.udf(flat, ArrayType(StringType()))
+        self.df = self.df.withColumn("categories", flat_udf(self.df.categories))\
+                        .withColumnRenamed("asin", "productid")\
+                        .withColumnRenamed("title", "productname")
+
+        self.df.write.jdbc(url=postgres_url, table='{}products'.format(self.category), mode='overwrite', properties=postgres_properties)
+
+
